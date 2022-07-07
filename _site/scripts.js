b4fb1c1aa78bd530f4009355da59b4f176d1eb6c
mershter <100219272+mershter@users.noreply.github.com>
2022-07-07 13:04:47 +0200

    working on podpor me page

--- _site/scripts.js
+++ _site/scripts.js
@@ -6,8 +6,8 @@ $('.hamburger-button').click(function(){
 
     $('.slider-container').slick({
         autoplay: true,        // Do we want it to autoplay? true or false
-          autoplaySpeed: 2000,   // How long between each slide when autoplaying
-          speed: 500,            // How fast is the transition 
+        autoplaySpeed: 2000,   // How long between each slide when autoplaying
+        speed: 500,            // How fast is the transition 
         arrows: true,          // Do you want to show arrows to trigger each slide
         accessibility: true,   // Enables tabbing and arrow key navigation 
         dots: true,            // Enables the dots below to show how many slides
@@ -18,7 +18,18 @@ $('.hamburger-button').click(function(){
         slidesToShow: 2,
         slidesToScroll: 1,
     });
-        
+
+let copyText = document.querySelector(".copy-text");
+copyText.querySelector("button").addEventListener("click",function(){
+  let input = copyText.querySelector("input.text");
+  input.select();
+  document.execCommand("copy");
+  copyText.classList.add("active");
+  window.getSelection().removeAllRanges();
+  setTimeout(function(){
+    copyText.classList.remove("active");
+  },2500);
+});
 
 // $('.slider-container').slick({
 //     autoplay: true,        // Do we want it to autoplay? true or false
